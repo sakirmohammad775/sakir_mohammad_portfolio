ed2fee05cb0f2ef1a0c6f8f9836dc91590f634ca
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-11-09 23:54:10 +0600

    add footer and newsletter

--- src/Home/NewsLetter/NewsLetter.jsx
+++ src/Home/NewsLetter/NewsLetter.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
+import bgImage from "../../assets/kevin-wang-JwR6ajxeShU-unsplash.jpg"
 
-const Newsletter=() =>{
+const Newsletter = () => {
   const [email, setEmail] = useState('');
 
   const handleSubmit = (e) => {
@@ -11,24 +12,32 @@ const Newsletter=() =>{
   };
 
   return (
-    <div className="bg-gray-800 text-white p-10">
-      <h2 className="text-3xl font-bold mb-4">Join Our Newsletter</h2>
-      <p className="text-lg mb-6">Stay updated with the latest news and offers.</p>
-      <form onSubmit={handleSubmit} className="flex">
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Enter your email"
-          className="flex-1 bg-gray-700 text-white p-3 rounded-l-md"
-        />
-        <button
-          type="submit"
-          className="bg-orange-500 hover:bg-orange-600 text-white px-4 py-3 rounded-r-md"
-        >
-          Subscribe
-        </button>
-      </form>
+    <div className="bg-gray-800 md:h-[200px] text-white p-10" style={{ backgroundImage: `url(${bgImage})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
+      <div className='max-w-screen-xl mx-auto grid lg:grid-cols-2'>
+        {/* left side */}
+        <div>
+          <h2 className="text-3xl font-bold mb-4">Join Our Newsletter</h2>
+          <p className="text-lg mb-6">Stay updated with the latest news and offers.</p>
+        </div>
+        {/**right side */}
+        <div>
+          <form onSubmit={handleSubmit} className="flex">
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Enter your email"
+              className="flex-1 bg-gray-700 text-white p-3 rounded-l-md"
+            />
+            <button
+              type="submit"
+              className="bg-orange-500 hover:bg-orange-600 text-white px-4 py-3 rounded-r-md"
+            >
+              Subscribe
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
